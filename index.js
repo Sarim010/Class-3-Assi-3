44c67f64cabe948ca6b0a2c135a23c918f9bcfd9
Sarim Raza <sarimraza23@gmail.com>
2024-03-31 15:13:08 +0500

    commit

---
+++ index.js
@@ -0,0 +1,47 @@
+"use strict";
+// Question 1
+let favMovie = "Wednesday";
+console.log("Fav Movie:" + "" + favMovie);
+// Question 2
+let intro = "Hi, I'm Sarim Raza, currently studying in the 3rd semester of BSCS.";
+console.log(intro);
+// Question 3
+let mood = true;
+console.log("Current Mood Happy:" + "" + mood);
+// Question 4
+let firstName = "Sarim";
+let lastName = "Raza";
+console.log(`${firstName} ${lastName}`);
+// Question 5
+let myAge = 25;
+console.log("My Age:" + "" + myAge);
+// Question 6
+let product = "Mehran Boss";
+let price = "$2500";
+console.log(`Product Name: ${product} Product Price: ${price}`);
+// Question 7
+let pet = 1;
+console.log("My Pet Quantity:" + "" + pet);
+// Question 8
+let myBirthday = true;
+console.log("Today My Birthday:" + "" + myBirthday);
+// Question 9
+let user_firstName = "First Name: Sarim";
+let user_lastName = "Last Name: Raza";
+let user_Age = "Age: 25";
+console.log(user_firstName);
+console.log(user_lastName);
+console.log(user_Age);
+// Question 10
+const birth_year = 2000;
+console.log("My Birth Year:" + "" + birth_year);
+// Question 11
+let country = "Country: Pakistan";
+let city = "City: Lahore";
+let full_adress = "Full Adress: Central Park Housing Socity Lahore";
+console.log(country);
+console.log(city);
+console.log(full_adress);
+// Question 12
+let todayTemperature = "Today Temperature: 30°C";
+console.log(todayTemperature);
---
+++ index.ts
@@ -0,0 +1,46 @@
+// Question 1
+let favMovie: string = "Wednesday";
+console.log("Fav Movie:" + "" + favMovie);
+// Question 2
+let intro: string = "Hi, I'm Sarim Raza, currently studying in the 3rd semester of BSCS.";
+console.log(intro);
+// Question 3
+let mood: boolean = true;
+console.log("Current Mood Happy:" + "" + mood);
+// Question 4
+let firstName = "Sarim";
+let lastName = "Raza";
+console.log(`${firstName} ${lastName}`);
+// Question 5
+let myAge: number = 25;
+console.log("My Age:" + "" + myAge);
+// Question 6
+let product: string = "Mehran Boss";
+let price: string = "$2500";
+console.log(`Product Name: ${product} Product Price: ${price}`);
+// Question 7
+let pet = 1;
+console.log("My Pet Quantity:" + "" + pet);
+// Question 8
+let myBirthday: boolean = true;
+console.log("Today My Birthday:" + "" + myBirthday);
+// Question 9
+let user_firstName: string = "First Name: Sarim";
+let user_lastName: string = "Last Name: Raza";
+let user_Age: string = "Age: 25";
+console.log(user_firstName);
+console.log(user_lastName);
+console.log(user_Age);
+// Question 10
+const birth_year: Number = 2000;
+console.log("My Birth Year:" + "" + birth_year);
+// Question 11
+let country: string = "Country: Pakistan";
+let city: string = "City: Lahore";
+let full_adress: string = "Full Adress: Central Park Housing Socity Lahore"
+console.log(country);
+console.log(city);
+console.log(full_adress);
+// Question 12
+let todayTemperature = "Today Temperature: 30°C";
+console.log(todayTemperature);
